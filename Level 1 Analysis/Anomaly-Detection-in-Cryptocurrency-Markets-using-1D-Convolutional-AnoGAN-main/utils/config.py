5125897eb790d2cfd3519ad8977e6ef58f82880a
christophknaden <cknaden@gmx.de>
2025-11-23 21:03:00 +0200

    add basic training capabilities for AnoGAN

---
+++ Level 1 Analysis/Anomaly-Detection-in-Cryptocurrency-Markets-using-1D-Convolutional-AnoGAN-main/utils/config.py
@@ -0,0 +1,48 @@
+import torch.nn as nn
+from torch.ao.nn.quantized.functional import threshold
+
+window_len = 30            # sliding window length
+step = 1                   # step size for sliding windows
+batch_size = 64            # dataloader batch size
+scaling_range = (-1, 1)    # MinMaxScaling range
+expected_features = 14     # multivariate time-series dimensionality
+
+
+data_path = "../data/test/ETH_1min.csv"
+device = "cpu"
+seq_len = 30
+
+#GAN configs
+z_dim = 100            # latent space dimension
+gf_dim = 128           # generator base filters
+df_dim = 32            # discriminator base filters
+channels = 5          # GAN input channels = number of time-series features
+
+
+#Training settings
+epochs_gan = 2        # GAN training epochs
+epochs_enc = 2        # Encoder training epochs
+lr_gan = 2e-4          # Adam lr for generator
+lr_disc = 1e-4         # Adam lr for discriminator
+lr_enc = 1e-4          # Adam lr for encoder
+betas = (0.5, 0.999)   # Adam betas for stability
+
+
+alpha_rec = 1.0        # reconstruction loss weight
+beta_feat = 0.1        # feature-matching loss weight
+
+
+#anomaly scoring
+anomaly_alpha = 0.9   
+
+#optional noise injection -- not implemented in the ready model -- 
+'''
+def add_noise(x, std=0.05):
+    """Apply Gaussian noise to stabilise D training."""
+    return x + torch.randn_like(x) * std
+'''
+
+
+bce_logits = nn.BCEWithLogitsLoss()  # loss for GAN
+mse_loss = nn.MSELoss()              # for encoder reconstruction + feature matching
+
